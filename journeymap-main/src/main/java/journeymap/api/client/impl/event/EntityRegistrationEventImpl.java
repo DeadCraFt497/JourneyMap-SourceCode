2fd01506e5b0359671fcf72e3c27aa42ad393172
DeadCraFt4967 <deadcraft_3@proton.me>
2025-05-18 08:46:20 -0500

    Delete journeymap-main directory

--- journeymap-main/src/main/java/journeymap/api/client/impl/event/EntityRegistrationEventImpl.java
+++
@@ -1,9 +0,0 @@
-package journeymap.api.client.impl.event;
-
-public class EntityRegistrationEventImpl {}
-
-
-/* Location:              C:\Users\zmm11\Downloads\jarfile\hack\journeymap-fabric-1.21.5-6.0.0-beta.46.jar!\journeymap\api\client\impl\event\EntityRegistrationEventImpl.class
- * Java compiler version: 21 (65.0)
- * JD-Core Version:       1.1.3
- */
